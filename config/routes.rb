3a12ee3e22005f62c2d6a0d03243fac75cd829f3
Kawish Hussain <kawish791@gmail.com>
2021-08-13 16:42:01 +0500

    beautify signup page

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
+  get "/welcome", to: "welcome#index"
   devise_for :users, controllers: {sessions: 'users/sessions', registrations: 'users/registrations'}
   root "welcome#index"
-  get "/welcome", to: "welcome#index"
 
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
