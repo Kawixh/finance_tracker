4fc663bcfe2536269441c23a52da1e1d39269aa8
Kawish Hussain <kawish791@gmail.com>
2021-08-16 13:04:42 +0500

    Added base for Stock functionality, fixed sign_out bug, created custom partial

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,13 @@
 Rails.application.routes.draw do
-  get "/welcome", to: "welcome#index"
-  get '/portfolio', to: 'users#portfolio'
   devise_for :users, controllers: {sessions: 'users/sessions', registrations: 'users/registrations'}
+  devise_scope :user do
+    get '/users/sign_out' => 'devise/sessions#destroy'
+  end
+  get "/welcome", to: "welcome#index"
+  # get "/welcome/:id", to: "welcome#index"
+  get '/portfolio', to: 'users#portfolio'
   root "welcome#index"
+
 
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
