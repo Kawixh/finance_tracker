de25311ca755aceef3492ac35a5918ba7d3bd619
Kawish Hussain <kawish791@gmail.com>
2021-08-16 11:19:51 +0500

    recoverd files

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,4 @@
 Rails.application.routes.draw do
-  get 'users/portfolio'
   get "/welcome", to: "welcome#index"
   get '/portfolio', to: 'users#portfolio'
   devise_for :users, controllers: {sessions: 'users/sessions', registrations: 'users/registrations'}
