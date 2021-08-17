226976d081f3b15a39a83ba6bf6e37233985388d
Kawish Hussain <kawish791@gmail.com>
2021-08-17 17:50:02 +0500

    Added friends functionality

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -3,4 +3,11 @@ class UsersController < ApplicationController
     @tracked_stocks = current_user.stocks
     @user = current_user
   end
+  def friends
+    @friends = current_user.friends
+
+  end
+  def search
+
+  end
 end
